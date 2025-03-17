32ad449d4a378fb1f362b4d6068de4c5dee21013
yatharth <yatharthmakvana@gmail.com>
2025-03-17 13:59:51 +0530

    Inserted parameters into function and created a for loop repeating the string n times.
    Also modified the test file to check the skipped tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text,times) {
+    let ans="";
+    if(times<0) return "ERROR";
+    for(let i=1;i<=times;i++){
+        ans+=text;
+    }
+    return ans;
 };
 
 // Do not edit below this line
