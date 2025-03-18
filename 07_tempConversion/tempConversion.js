a59910e01e5ce2e7b66a76999b14c1372df2f3fd
yatharth <yatharthmakvana@gmail.com>
2025-03-18 09:46:48 +0530

    Wrote the functions for converting Temperature Scales.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celcius=Math.round(((temp-32)*5/9)*10)/10;
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let farhen=Math.round((temp*9/5+32)*10)/10;
+  return farhen;
 };
 
 // Do not edit below this line
