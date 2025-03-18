fdc149b16574d5525eab1f4bc2dd84d293dae0c5
yatharth <yatharthmakvana@gmail.com>
2025-03-18 09:12:12 +0530

    Added all values between given parameters.
    Also checked if the given parameters were integers
    with Number.isInteger() function.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    if(num1<0 || num2<0 || !Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    let sum=0;
+    let start;
+    let end;
+    if(num1<num2){
+        start=num1;
+        end=num2;
+    }
+    else{
+        start=num2;
+        end=num1;
+    }
 
+    for(let i=start;i<=end;i++){
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
